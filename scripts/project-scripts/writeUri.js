c58ebc60268676ac23ac297d0f95b7c4d9385bfa
Aad Versteden <madnificent@gmail.com>
2020-09-11 00:08:01 +0200

    Introduce script for adding URIs to the document
    The script could be cleaned up and made more flexible.  This is mainly
    a starting point for what we had.

--- scripts/project-scripts/writeUri.js
+++ scripts/project-scripts/writeUri.js
@@ -3,15 +3,15 @@ const { v4: uuid } = require('uuid');
 const fs = require('fs');
 
 async function main(){
-  var fileName='./toevla-latest.xlsx';
-  var sheetName='Moederdoc_29-06-2020';
+  var fileName='/data/app/toevla-latest.xlsx';
+  var sheetName='Moederdoc';
   
   var workbook = new ExcelJS.Workbook();
   await workbook.xlsx.readFile(fileName);
   var worksheet=workbook.getWorksheet(sheetName);
 
   var baseUrl='https://data.toevla.org/id/concepts/';
-  var range={start: 5, end: 332}
+  var range={start: 5, end: 332};
 
   var cell;
   
