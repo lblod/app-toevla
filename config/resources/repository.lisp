b5d60262bb68ff30dae17b42fd13301b3be17ce9
Aad Versteden <madnificent@gmail.com>
2020-10-12 19:32:27 +0200

    Fix rdfs prefix
    The prefix for the rdfs prefix was incorrect.  This has been correct
    with the current code updated and corresponding migration update.

--- config/resources/repository.lisp
+++ config/resources/repository.lisp
@@ -34,7 +34,7 @@
 (add-prefix "ext" "http://mu.semte.ch/vocabularies/ext/")
 (add-prefix "skos" "http://www.w3.org/2004/02/skos/core#")
 (add-prefix "dct" "http://purl.org/dc/terms/")
-(add-prefix "rdfs" "http://www.w3.org/2000/01/rdf-schema#label")
+(add-prefix "rdfs" "http://www.w3.org/2000/01/rdf-schema#")
 (add-prefix "toevla" "http://toevla.org/ns/generic/")
 (add-prefix "schema" "http://schema.org/")
 (add-prefix "nfo" "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#")
