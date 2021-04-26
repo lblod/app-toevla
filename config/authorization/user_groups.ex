10b7d1207e588fae15e7c5a7fe2728ae74ee906c
Aad Versteden <madnificent@gmail.com>
2021-04-26 23:47:37 +0200

    Upgrade user-groups for access-rights systems
    The new access rights system is more complex than the older systems.  In
    the new system we don't count on ACM/IDM but rather count on us
    controlling the full access rights, including the mutliple roles an
    individual may have.

--- config/authorization/user_groups.ex
+++ config/authorization/user_groups.ex
@@ -9,42 +9,188 @@ alias Acl.GroupSpec, as: GroupSpec
 alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
 
 defmodule Acl.UserGroups.Config do
+  @known_resource_types [
+    "http://www.w3.org/2004/02/skos/core#ConceptScheme",
+    "http://www.w3.org/2004/02/skos/core#Concept",
+    "https://data.vlaanderen.be/ns/adres#AdresseerbaarObject",
+    "http://schema.org/Parking",
+    "http://toevla.org/ns/generic/Entrance",
+    "http://toevla.org/ns/generic/GuidedTour",
+    "http://toevla.org/ns/generic/Auditorium",
+    "https://linkedgeodata.org/ontology/Toilets",
+    "http://toevla.org/ns/generic/Area",
+    "http://toevla.org/ns/generic/Experience",
+    "http://toevla.org/ns/generic/Route",
+    "http://toevla.org/ns/generic/TreeNodeScore",
+    "http://toevla.org/ns/generic/Scorable",
+    "http://toevla.org/ns/generic/Path",
+    "http://toevla.org/ns/generic/RouteDescription",
+    "http://toevla.org/ns/generic/TrainStop",
+    "http://toevla.org/ns/generic/BusStop",
+    "http://toevla.org/ns/generic/TramStop",
+    "http://toevla.org/ns/generic/Restaurant",
+    "http://toevla.org/ns/generic/Shop",
+    "http://toevla.org/ns/generic/Widget",
+    "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject"
+  ]
+
+  @museum_types [
+    "https://data.vlaanderen.be/ns/adres#AdresseerbaarObject",
+    "http://schema.org/Parking",
+    "http://toevla.org/ns/generic/Entrance",
+    "http://toevla.org/ns/generic/GuidedTour",
+    "http://toevla.org/ns/generic/Auditorium",
+    "https://linkedgeodata.org/ontology/Toilets",
+    "http://toevla.org/ns/generic/Area",
+    "http://toevla.org/ns/generic/Experience",
+    "http://toevla.org/ns/generic/Route",
+    "http://toevla.org/ns/generic/TreeNodeScore",
+    "http://toevla.org/ns/generic/Scorable",
+    "http://toevla.org/ns/generic/Path",
+    "http://toevla.org/ns/generic/RouteDescription",
+    "http://toevla.org/ns/generic/TrainStop",
+    "http://toevla.org/ns/generic/BusStop",
+    "http://toevla.org/ns/generic/TramStop",
+    "http://toevla.org/ns/generic/Restaurant",
+    "http://toevla.org/ns/generic/Shop",
+    "http://toevla.org/ns/generic/Widget",
+    "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject"
+  ]
+
+  @session_types [
+    "http://mu.semte.ch/vocabularies/session/Session"
+  ]
+
+  @account_types [
+    "http://xmlns.com/foaf/0.1/Person",
+    "http://xmlns.com/foaf/0.1/OnlineAccount",
+    "http://mu.semte.ch/vocabularies/ext/Role",
+    "http://mu.semte.ch/vocabularies/ext/ValidatorRole",
+    "http://mu.semte.ch/vocabularies/ext/DataEntryRole"
+  ]
+
+  @shared_readonly [
+    "http://www.w3.org/2004/02/skos/core#ConceptScheme",
+    "http://www.w3.org/2004/02/skos/core#Concept"
+  ]
+
   def user_groups do
     [
-      # // PUBLIC
+      # Accounts
+      %GroupSpec{
+        name: "account",
+        # TODO: remove :write and ensure writing happens through custom
+        # service
+        useage: [:read, :read_for_write,:write],
+        access: %AccessByQuery{
+          query: "PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+                  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+                  PREFIX musession: <http://mu.semte.ch/vocabularies/session/>
+                  SELECT ?id WHERE {
+                    <SESSION_ID> ext:hasAccount/mu:uuid ?id.
+                  }",
+          vars: ["id"]
+        },
+        graphs: [
+          %GraphSpec{
+            graph: "http://data.toevla.org/accounts/",
+            constraint: %ResourceConstraint{
+              resource_types: @account_types ++ @session_types
+            }
+          }
+        ]
+      },
+
+      # Museum
+      %GroupSpec{
+        name: "museum",
+        useage: [:read, :write, :read_for_write],
+        access: %AccessByQuery{
+          query: "PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+                  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+                  PREFIX musession: <http://mu.semte.ch/vocabularies/session/>
+                  SELECT ?id WHERE {
+                    <SESSION_ID> ext:hasRole/ext:actsOn/mu:uuid ?id.
+                  }",
+          vars: ["id"]
+        },
+        graphs: [
+          %GraphSpec{
+            graph: "http://data.toevla.org/musea/",
+            constraint: %ResourceConstraint{
+              resource_types: @museum_types
+            }
+          }
+        ]
+      },
+
+      # Inter
+      %GroupSpec{
+        name: "inter",
+        useage: [:read, :write, :read_for_write],
+        access: %AccessByQuery{
+          query: "PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+                  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+                  PREFIX musession: <http://mu.semte.ch/vocabularies/session/>
+                  SELECT * WHERE {
+                    <SESSION_ID> ext:hasRole/a ext:ValidatorRole.
+                  }",
+          vars: []
+        },
+        graphs: [
+          %GraphSpec{
+            graph: "http://data.toevla.org/inter",
+            constraint: %ResourceConstraint{
+              resource_types: @account_types ++ @museum_types
+            }
+          }
+        ]
+      },
+
+      # Shared knowledge
+      %GroupSpec{
+        name: "shared",
+        useage: [:read],
+        access: %AlwaysAccessible{},
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/shared", # TODO: can become public in the future
+            constraint: %ResourceConstraint{
+              resource_types: [
+                "http://www.w3.org/2004/02/skos/core#ConceptScheme",
+                "http://www.w3.org/2004/02/skos/core#Concept"
+              ]
+            }
+          }
+        ]
+      },
+
+      # OLD PUBLIC
       %GroupSpec{
         name: "public",
-        useage: [:read,:write,:read_for_write],
-        access: %AlwaysAccessible{}, # TODO: Should be only for logged in users
-        graphs: [ %GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/public",
-                    constraint: %ResourceConstraint{
-                      resource_types: [
-                        "http://www.w3.org/2004/02/skos/core#ConceptScheme",
-                        "http://www.w3.org/2004/02/skos/core#Concept",
-                        "https://data.vlaanderen.be/ns/adres#AdresseerbaarObject",
-                        "http://schema.org/Parking",
-                        "http://toevla.org/ns/generic/Entrance",
-                        "http://toevla.org/ns/generic/GuidedTour",
-                        "http://toevla.org/ns/generic/Auditorium",
-                        "https://linkedgeodata.org/ontology/Toilets",
-                        "http://toevla.org/ns/generic/Area",
-                        "http://toevla.org/ns/generic/Experience",
-                        "http://toevla.org/ns/generic/Route",
-                        "http://toevla.org/ns/generic/TreeNodeScore",
-                        "http://toevla.org/ns/generic/Scorable",
-                        "http://toevla.org/ns/generic/Path",
-                        "http://toevla.org/ns/generic/RouteDescription",
-                        "http://toevla.org/ns/generic/TrainStop",
-                        "http://toevla.org/ns/generic/BusStop",
-                        "http://toevla.org/ns/generic/TramStop",
-                        "http://toevla.org/ns/generic/Restaurant",
-                        "http://toevla.org/ns/generic/Shop",
-                        "http://toevla.org/ns/generic/Widget",
-                        "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject"
-                      ]
-                    } }
-                ] },
+        useage: [:read, :write, :read_for_write],
+        access: %AccessByQuery{
+          query: "PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+                  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+                  PREFIX musession: <http://mu.semte.ch/vocabularies/session/>
+                  SELECT ?s WHERE {
+                    ?s ?p ?o.
+                    FILTER NOT EXISTS {
+                      <SESSION_ID> ext:hasAccount ?account.
+                    }
+                  } LIMIT 1",
+          vars: []
+        },
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/public",
+            constraint: %ResourceConstraint{
+              resource_types: @known_resource_types
+            }
+          }
+        ]
+      },
+
       # // CLEANUP
       #
       %GraphCleanup{
