9ba8984efc681b85bce01a7f8d9625251a64a799
Aad Versteden <madnificent@gmail.com>
2021-04-26 23:43:23 +0200

    Upgrade dispatcher rules
    This is a large and longstanding update to upgrade the dispatcher
    rules.  The high-level idea is that the dispatcher now relies more on
    content-negatiation and that the layers would be easier to identify.

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -8,11 +8,24 @@ defmodule Dispatcher do
     any: [ "*/*" ]
   ]
 
-  define_layers [ :cors, :widget_embedding, :static_with_host, :static, :frontend_and_api_routes, :api, :frontend_fallback_with_host, :frontend_fallback, :not_found ]
+  define_layers [ :cors, :widget_embedding, :static_with_host, :static, :api, :frontend_fallback_with_host, :frontend_fallback, :not_found ]
 
   @json_service %{ accept: [:json], layer: :api }
   @image %{ accept: [:image] }
-  # @html %{ accept: %{ html: true } }
+
+
+  ### GENERAL CATCHES for further cleaning
+
+  match "/.well-known/*_path", _ do
+    send_resp( conn, 200, "{ \"message\": \"ok\" }" )
+  end
+
+  match "/favicon.ico/*_path", _ do
+    send_resp( conn, 404, "No icon specified" )
+  end
+
+
+  ### CORS
 
   options "/*_path", %{ layer: :cors } do
     conn
@@ -21,6 +34,71 @@ defmodule Dispatcher do
     |> send_resp( 200, "{ \"message\": \"ok\" }" )
   end
 
+
+  ###
+  ## WIDGET EMBEDDING
+  ##
+  ## Some overrides for widget paths so we can run the widget from the
+  ## data entry path.
+
+  match "/assets/widget/initialize-widget.js", %{ layer: :widget_embedding } do
+    Proxy.forward conn, [], "http://frontend-embed/assets/js/initialize-widget.js"
+  end
+
+  match "/assets/widget/*path", %{ layer: :widget_embedding } do
+    Proxy.forward conn, path, "http://frontend-embed/assets/"
+  end
+
+
+  ###
+  ## STATIC
+  ##
+  ## Normally static resources which we can only fetch through using the
+  ## next path because they lack the right Accept Type
+
+  match "/assets/*path", %{ reverse_host: ["widget" | _rest], layer: :static } do
+    Proxy.forward conn, path, "http://frontend-standalone/assets/"
+  end
+
+  match "/@appuniversum/*path", %{ reverse_host: ["widget" | _rest], layer: :static } do
+    Proxy.forward conn, path, "http://frontend-standalone/@appuniversum/"
+  end
+
+  match "/assets/*path", %{ layer: :static } do
+    Proxy.forward conn, path, "http://frontend-entry/assets/"
+  end
+
+  match "/@appuniversum/*path", %{ layer: :static } do
+    Proxy.forward conn, path, "http://frontend-entry/@appuniversum/"
+  end
+
+  match "/images/*path", %{ layer: :static } do
+    Proxy.forward conn, path, "http://imageservice/image/"
+  end
+
+
+  ###
+  ## CONTENT-NEGOTIATED ROUTES
+  ##
+  ## Some routes can be accessed either from the frontend or from the
+  ## API.  Content negotiation will yield the right routes.  Basically,
+  ## we should prefer the HTML if HTML is accepted so humans can see the
+  ## right content.  Otherwise we yield the JSON response with the
+  ## logical content.
+  ##
+  ## We position these together with content-negotiated routes for which
+  ## only a single response should be supplied.
+
+  ## Combined routes
+  match "/widgets/*_path", %{ layer: :api, accept: %{ html: true } } do
+    Proxy.forward conn, [], "http://frontend-standalone/index.html"
+  end
+
+  match "/widgets/*path", %{ layer: :api, accept: %{ json: true } } do
+    Proxy.forward conn, path, "http://cache/widgets/"
+  end
+
+  ## Api Services
   match "/concept-schemes/*path", @json_service do
     Proxy.forward conn, path, "http://cache/concept-schemes/"
   end
@@ -33,10 +111,6 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://cache/points-of-interest/"
   end
 
-  match "/scorables/*path", @json_service do
-    Proxy.forward conn, path, "http://cache/scorables/"
-  end
-
   match "/experiences/*path", @json_service do
     Proxy.forward conn, path, "http://cache/experiences/"
   end
@@ -47,6 +121,10 @@ defmodule Dispatcher do
 
   match "/experience-tree-node-scores/*path", @json_service do
     Proxy.forward conn, path, "http://cache/experience-tree-node-scores/"
+  end
+
+  match "/scorables/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/scorables/"
   end
 
   match "/toilets/*path", @json_service do
@@ -105,79 +183,62 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://cache/shops/"
   end
 
-  match "/images/*path", %{ layer: :static } do
-    Proxy.forward conn, path, "http://imageservice/image/"
+  match "/people/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/people/"
+  end
+
+  match "/accounts/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/accounts/"
   end
 
   match "/file-service/files/*path", %{ layer: :api } do
     Proxy.forward conn, path, "http://file/files/"
   end
 
-  match "/sessions/*path", %{ layer: :api } do
-    Proxy.forward conn, path, "http://toevlaacmidmloginservice/sessions/"
+  match "/mocklogin/*path", @json_service do
+    Proxy.forward conn, path, "http://mocklogin/"
   end
 
-  match "/favicon.ico/*_path", _ do
-    send_resp( conn, 404, "No icon specified" )
-  end    
-
-  match "/assets/*path", %{ reverse_host: ["standalone" | _rest ], layer: :static_with_host } do
-    Proxy.forward conn, path, "http://frontend-standalone/assets/"
+  post "/sessions/role/*path", @json_service do
+    Proxy.forward conn, [], "http://switchrole/sessions/role"
+  end
+  match "/sessions/*path", @json_service do
+    # We go to resource as we expect this might create race conditions
+    # with the delta messages otherwise.
+    Proxy.forward conn, path, "http://resource/sessions/"
+  end
+  match "/roles/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/roles/"
+  end
+  match "/validator-roles/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/validator-roles/"
+  end
+  match "/data-entry-roles/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/data-entry-roles/"
+  end
+  match "/accounts/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/accounts/"
   end
 
-  match "/assets/*path", %{ reverse_host: ["entry" | _rest ], layer: :static_with_host } do
-    Proxy.forward conn, path, "http://frontend-entry/assets/"
-  end
 
-  match "/authorization/callback/*_rest", %{ host: "*.toegankelijk.vlaanderen.be", layer: :static_with_host } do
-    Proxy.forward conn, [], "http://frontend-entry/torii/redirect.html"
-  end
+  # FRONTEND FALLBACK WITH HOST
 
-  match "/widgets/*_path", %{ layer: :frontend_and_api_routes, accept: %{ html: true } } do
+  match "/*path", %{ reverse_host: ["widget" | _rest], layer: :frontend_fallback } do
     Proxy.forward conn, [], "http://frontend-standalone/index.html"
-  end
-
-  match "/widgets/*path", %{ layer: :frontend_and_api_routes, accept: %{ json: true } } do
-    Proxy.forward conn, path, "http://cache/widgets/"
-  end
-
-  match "/assets/widget/initialize-widget.js", %{ layer: :widget_embedding } do
-    Proxy.forward conn, [], "http://frontend-embed/assets/js/initialize-widget.js"
-  end
-
-  match "/assets/widget/*path", %{ layer: :widget_embedding } do
-    Proxy.forward conn, path, "http://frontend-embed/assets/"
-  end
-
-  match "/assets/*path", %{ layer: :static } do
-    Proxy.forward conn, path, "http://frontend-entry/assets/"
-  end
-
-  match "/@appuniversum/*path", %{ layer: :static } do
-    Proxy.forward conn, path, "http://frontend-entry/@appuniversum/"
-  end
-
-  match "/@appuniversum/*path", %{ reverse_host: ["standalone" | _rest ], layer: :static_with_host } do
-    Proxy.forward conn, path, "http://frontend-standalone/@appuniversum/"
-  end
-
-  match "/@appuniversum/*path", %{ reverse_host: ["entry" | _rest ], layer: :static_with_host } do
-    Proxy.forward conn, path, "http://frontend-entry/@appuniversum/"
-  end
-
-  match "/*path", %{ reverse_host: ["standalone" | _rest], layer: :frontend_fallback_with_host } do
-    Proxy.forward conn, path, "http://frontend-standalone/index.html"
-  end
-
-  match "/*path", %{ reverse_host: ["entry" | _rest], layer: :frontend_fallback_with_host } do
-    Proxy.forward conn, path, "http://frontend-entry/index.html"
   end
 
   match "/*path", %{ layer: :frontend_fallback } do
     Proxy.forward conn, [], "http://frontend-entry/index.html"
   end
 
-  match "/*_path", %{ last_call: true } do
-    send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
+
+  # 404
+
+  match "/*_path", %{ layer: :not_found } do
+    send_resp( conn, 404, "{ \"errors\": [{\"status\": \"Not found\", \"code\": \"404\", \"title\": \"The file could not be found\"}] }" )
+  end
+
+  match "/*_path", %{ layer: :not_found } do
+    send_resp( conn, 404, "Route not found and no applicable 404 method discovered for your accept headers." )
   end
 end
