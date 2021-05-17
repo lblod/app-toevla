ba4d5453628115a223f86bf9175674497ae8a79c
Aad Versteden <madnificent@gmail.com>
2021-05-18 00:10:51 +0200

    Update importer for new graphs
    The public graph is now called the shared graph.  This name change
    should be reflected in the migrations.

--- scripts/project-scripts/generate-ttl.sh
+++ scripts/project-scripts/generate-ttl.sh
@@ -8,7 +8,7 @@ NEW_TREE_TIMESTAMP=`date +%Y%m%d%H%M%S`;
 echo "Creating clear migrations";
 cp ./clear_migration_template.sparql /data/app/config/migrations/$CLEAR_TREE_TIMESTAMP-clear-old-tree.sparql
 cp /data/app/generated.ttl /data/app/config/migrations/$NEW_TREE_TIMESTAMP-new-tree.ttl
-echo "http://mu.semte.ch/graphs/public" > /data/app/config/migrations/$NEW_TREE_TIMESTAMP-new-tree.graph
+echo "http://mu.semte.ch/graphs/shared" > /data/app/config/migrations/$NEW_TREE_TIMESTAMP-new-tree.graph
 echo ""
 echo "Created migrations:
  - ./config/migrations/$CLEAR_TREE_TIMESTAMP-clear-old-tree.sparql
